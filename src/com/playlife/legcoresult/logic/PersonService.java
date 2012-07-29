c98dec9b4214f4bf5c21a9543c1aa90519e1bcda
razgrizbw <razgrizbw@legcoresult>
2012-07-30 00:08:25 +0800

    - PersonService.java, IPersonDAO.java 1 [New] add interface to access person data

---
+++ src/com/playlife/legcoresult/logic/PersonService.java
@@ -0,0 +1,15 @@
+package com.playlife.legcoresult.logic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.playlife.legcoresult.persistence.daos.IPersonDAO;
+import com.playlife.legcoresult.persistence.domainobjects.Person;
+
+public class PersonService {
+	@Autowired
+	IPersonDAO personDAO;
+	
+	public Person getById(Long id){
+		return personDAO.get(id);
+	}
+}
---
+++ src/com/playlife/legcoresult/persistence/daos/IPersonDAO.java
@@ -0,0 +1,8 @@
+package com.playlife.legcoresult.persistence.daos;
+
+import com.playlife.legcoresult.persistence.daos.generic.IGenericDAO;
+import com.playlife.legcoresult.persistence.domainobjects.Person;
+
+public interface IPersonDAO extends IGenericDAO<Person, Long> {
+
+}
